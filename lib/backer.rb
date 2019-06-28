066460cdf17934b22b57c2189c838d3910881a1b
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-28 22:38:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,4 +1,5 @@
 class Backer
+<<<<<<< HEAD
 attr_accessor :backed_projects, :name 
 
 def initialize(name)
@@ -10,5 +11,14 @@ def back_project(project)
   @backed_projects << project
   project.backers << self
 end 
+=======
+attr_reader :name 
+
+@@backed_projects
+def initialize(name)
+  @name = name 
+  @@backed_projects << self
+end
+>>>>>>> e15aeb265cef03ef828b00294d07d77404881288
   
 end 
